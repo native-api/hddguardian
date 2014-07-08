56677b4c4acef290c2c308945cefcd41b2a04f64
SND\Eruestan_cp <SND\Eruestan_cp@2727b4ef-30a4-488d-b0fc-cb09fe5562a7>
2014-07-08 20:55:22 +0000

    git-svn-id: https://hddguardian.svn.codeplex.com/svn@35388 2727b4ef-30a4-488d-b0fc-cb09fe5562a7

---
+++ Source/0.6/0.6.0/HDD Guardian/5 - About/About.vb
@@ -0,0 +1,33 @@
+﻿'HDD Guardian is a GUI for smartcl utility, part of smartmontools
+'
+'home pages are http://code.google.com/p/hddguardian/
+'               http://hddguardian.codeplex.com/
+'
+'Copyright (C) 2010-2014 Samuele Parise
+'
+'This Source Code Form is subject to the terms of the Mozilla Public
+'License, v. 2.0. If a copy of the MPL was not distributed with this
+'file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Public Class About
+
+    Private Sub ExternalLink(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles _
+        lnkHddGuardian.LinkClicked, _
+        lnkGoogleCode.LinkClicked, lnkCodePlex.LinkClicked, lnkGroup.LinkClicked, lnkEmail.LinkClicked, lnkPlus.LinkClicked, _
+        lnkSmartMonTools.LinkClicked, lnkGpl.LinkClicked, _
+        lnkKamiyamane.LinkClicked, lnkCcBy.LinkClicked, _
+        lnkOxygen.LinkClicked, lnkLgplOxygen.LinkClicked, _
+        lnkCrystal.LinkClicked, lnkEveraldo.LinkClicked, lnkLgplCrystal.LinkClicked, _
+        lnkOpenClipart.LinkClicked, _
+        lnkBrandsOfTheWorld.LinkClicked, _
+        lnkWesternDigital.LinkClicked, _
+        lnkCoolerMaster.LinkClicked, _
+        lnkOxygen2.LinkClicked
+
+        Try
+            System.Diagnostics.Process.Start(sender.Tag)
+        Catch ex As Exception
+            Main.PrintDebug("External link error: " & ex.ToString, True)
+        End Try
+    End Sub
+End Class
