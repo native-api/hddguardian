902feffea026b1e0b62dd3bfede1f42f60219426
SND\Eruestan_cp <SND\Eruestan_cp@2727b4ef-30a4-488d-b0fc-cb09fe5562a7>
2014-08-19 20:58:35 +0000

    git-svn-id: https://hddguardian.svn.codeplex.com/svn@36229 2727b4ef-30a4-488d-b0fc-cb09fe5562a7

---
+++ Source/0.6/0.6.1/HDD Guardian/1 - Main form/Main form partial classes/Chart.vb
@@ -0,0 +1,143 @@
+﻿'HDD Guardian is a GUI for smartcl utility, part of smartmontools
+'
+'home pages are http://code.google.com/p/hddguardian/
+'               http://hddguardian.codeplex.com/
+'
+'Copyright (C) 2010-2014 Samuele Parise
+'
+'This Source Code Form is subject to the terms of the Mozilla Public
+'License, v. 2.0. If a copy of the MPL was not distributed with this
+'file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Imports System.Xml
+
+Partial Class Main
+
+    Private Sub SetGraph()
+        Dim dev As Device = devlist(cboDevices.SelectedIndex)
+
+        If Not IsNothing(dev.Log) Then
+            With chrChart
+                .Series(0).Points.Clear()
+                .Series(0).MarkerSize = 0
+
+                For i As Short = 0 To dev.Log.Count - 1
+                    If dev.Log(i).EventName = cboChartEvents.SelectedItem Then
+                        Dim p As New DataVisualization.Charting.DataPoint
+                        p.YValues(0) = Convert.ToDouble(dev.Log(i).OldValue.Replace("°C", ""))
+
+                        .Series(0).Points.Add(p)
+                        Exit For
+                    End If
+                Next
+
+                For i As Short = 0 To dev.Log.Count - 1
+                    If dev.Log(i).EventName = cboChartEvents.SelectedItem Then
+                        Dim p As New DataVisualization.Charting.DataPoint
+                        p.YValues(0) = Convert.ToDouble(dev.Log(i).NewValue.Replace("°C", ""))
+
+                        .Series(0).Points.Add(p)
+                    End If
+                Next
+            End With
+        End If
+
+    End Sub
+
+    Private Sub FillList()
+        Dim dev As Device = devlist(cboDevices.SelectedIndex)
+
+        If Not IsNothing(dev.Log) Then
+            If dev.Log.Count > 0 Then
+                With lvwChart
+                    .Items.Clear()
+                    For i As Short = 0 To dev.Log.Count - 1
+                        If dev.Log(i).EventName = cboChartEvents.SelectedItem Then
+
+                            Dim y As String = dev.Log(i).Year
+                            Dim m As String = String.Format("{0:D2}", Convert.ToInt16(dev.Log(i).Month))
+                            Dim d As String = String.Format("{0:D2}", Convert.ToInt16(dev.Log(i).Day))
+                            Dim t() As String = dev.Log(i).Hour.Split(":")
+                            Dim h As String = String.Format("{0:D2}", Convert.ToInt16(t(0))) & ":" & _
+                                String.Format("{0:D2}", Convert.ToInt16(t(1))) & ":" & _
+                                String.Format("{0:D2}", Convert.ToInt16(t(2)))
+
+                            Dim timeofevent As String = ""
+
+                            Dim dt As DateTime
+                            If DateTime.TryParseExact(d & " " & m & " " & y & " " & h, "dd MM yyyy HH:mm:ss", Nothing, Nothing, dt) Then
+                                timeofevent = FormatDateTime(dt, DateFormat.ShortDate) & ", " & FormatDateTime(dt, DateFormat.ShortTime)
+                            End If
+
+                            .Items.Add(timeofevent)
+
+                            With .Items(.Items.Count - 1).SubItems
+                                .Add(dev.Log(i).OldValue.Replace("°C", ""))
+                                .Add(dev.Log(i).NewValue.Replace("°C", ""))
+
+                                Dim variation As String = dev.Log(i).Variation.Replace("°C", "")
+
+                                If IsNumeric(variation) Then
+                                    If Convert.ToDouble(variation) > 0 Then
+                                        .Add("+" & dev.Log(i).Variation.Replace("°C", ""))
+                                    Else
+                                        .Add(dev.Log(i).Variation.Replace("°C", ""))
+                                    End If
+                                End If
+
+                            End With
+
+                        End If
+                    Next
+                    .Columns(0).AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+                    lblEvents.Text = lblEvents.Tag.ToString.Replace("%", .Items.Count)
+                End With
+            End If
+        End If
+
+    End Sub
+
+    Public Sub FillChartEvents()
+        Dim dev As Device = devlist(cboDevices.SelectedIndex)
+
+        tlpChart.Visible = False
+        lvwChart.Visible = False
+        chrChart.Visible = False
+        lblChartCount.Visible = True
+        lblChartCount.Text = m_nologfordevice
+
+        If Not IsNothing(dev.Log) Then
+            If dev.Log.Count > 0 Then
+
+                With cboChartEvents
+                    .Items.Clear()
+
+                    For i As Short = 0 To dev.Log.Count - 1
+
+                        Dim notduplicate As Boolean = True
+
+                        For l As Short = 0 To .Items.Count - 1
+                            If dev.Log(i).EventName = .Items(l) Then
+                                notduplicate = False
+                                Exit For
+                            End If
+                        Next
+
+                        If notduplicate Then
+                            .Items.Add(dev.Log(i).EventName)
+                        End If
+                    Next
+
+                    If .Items.Count > 0 Then .SelectedIndex = 0
+
+                End With
+
+                tlpChart.Visible = True
+                lvwChart.Visible = True
+                chrChart.Visible = True
+                lblChartCount.Visible = False
+            End If
+        End If
+
+    End Sub
+End Class
---
+++ Source/0.6/0.6.1/HDD Guardian/1 - Main form/Main form partial classes/DeviceSettings.vb
@@ -0,0 +1,137 @@
+﻿'HDD Guardian is a GUI for smartcl utility, part of smartmontools
+'
+'home pages are http://code.google.com/p/hddguardian/
+'               http://hddguardian.codeplex.com/
+'
+'Copyright (C) 2010-2014 Samuele Parise
+'
+'This Source Code Form is subject to the terms of the Mozilla Public
+'License, v. 2.0. If a copy of the MPL was not distributed with this
+'file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Imports System.Xml
+
+Partial Class Main
+
+    Dim allusersfolder As String = My.Computer.FileSystem.SpecialDirectories.AllUsersApplicationData '& "\"
+    Dim curruserfolder As String = My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData '& "\"
+
+    Public Sub LoadDeviceSettings(ByVal device As Device)
+        If Not IsNothing(device.Options) Then
+            If device.Options.Length > 0 Then
+                ParseOptions(device.Options)
+            End If
+        End If
+    End Sub
+
+    Public Sub SaveDeviceSettings()
+        If isloading_devsettings Then Exit Sub
+
+        If lvwDevices.Items.Count = 0 Then Exit Sub
+        Dim dev As Device = devlist(cboDevices.SelectedIndex)
+
+        Dim attrformat As String = ""
+        If chkAttributes.Checked Then
+            With lvwAttrFormat
+                If .Items.Count > 0 Then
+                    For i As Integer = 0 To .Items.Count - 1
+                        attrformat = attrformat & "-v " & .Items(i).Text & "," & _
+                        .Items(i).SubItems(1).Text & "," & .Items(i).SubItems(2).Text & " "
+                    Next
+                End If
+            End With
+        End If
+
+        Dim fdebug As String = ""
+        Dim swap As String = ""
+        Dim nologdir As String = ""
+        Dim xerror As String = ""
+        If chkFirmware.Checked Then
+            Select Case cboFirmware.SelectedIndex
+                Case 0
+                    fdebug = "-F none "
+                Case 1
+                    fdebug = "-F samsung "
+                Case 2
+                    fdebug = "-F samsung2 "
+                Case 3
+                    fdebug = "-F samsung3 "
+            End Select
+            If chkFixSwap.Checked Then swap = "-F swapid "
+            If chkNoLogDir.Checked Then nologdir = "-F nologdir "
+            If chkXErrorLba.Checked Then xerror = "-F xerrorlba "
+        End If
+
+        Dim attrautosave As String = ""
+        If chkEnableAutosave.Checked = True Then attrautosave = "-S on "
+
+        Dim content As String = attrformat & fdebug & swap & nologdir & xerror & attrautosave
+
+        dev.Options = content
+
+        Dim standby As String = ""
+        If trkStandby.Value > 0 Then dev.Standby = trkStandby.Value
+    End Sub
+
+
+    Public Sub ParseOptions(ByVal options As String)
+        isloading_devsettings = True 'this prevent a not required saving for device settings
+
+        chkEnableAutosave.Checked = False
+        chkAttributes.Checked = False
+        chkFirmware.Checked = False
+        lvwAttrFormat.Items.Clear()
+
+        If IsNothing(options) Then
+            isloading_devsettings = False
+            Exit Sub
+        End If
+
+        Dim commands() As String = options.Split("-", 255, StringSplitOptions.RemoveEmptyEntries)
+
+        chkAttributes.Enabled = True
+        chkFirmware.Enabled = True
+
+        For i As Integer = 0 To commands.Length - 1
+            Dim Param() As Char = commands(i).Trim.ToCharArray
+            If Param.Length > 0 Then
+                Select Case Param(0)
+                    Case "v"
+                        chkAttributes.Checked = True
+                        Dim Attribute() As String = commands(i).Substring(1).Trim.Split(",")
+                        If Attribute.Length = 2 Then
+                            With lvwAttrFormat
+                                .Items.Add(Attribute(0))
+                                .Items(.Items.Count - 1).SubItems.Add(Attribute(1))
+                            End With
+                        Else
+                            With lvwAttrFormat
+                                .Items.Add(Attribute(0))
+                                .Items(.Items.Count - 1).SubItems.Add(Attribute(1))
+                                .Items(.Items.Count - 1).SubItems.Add(Attribute(2))
+                            End With
+                        End If
+                    Case "F"
+                        chkFirmware.Checked = True
+                        If commands(i).Contains("none") Then cboFirmware.SelectedIndex = 0
+                        If commands(i).Contains("samsung") Then cboFirmware.SelectedIndex = 1
+                        If commands(i).Contains("samsung2") Then cboFirmware.SelectedIndex = 2
+                        If commands(i).Contains("samsung3") Then cboFirmware.SelectedIndex = 3
+                        If commands(i).Contains("swapid") Then chkFixSwap.Checked = True
+                        If commands(i).Contains("nologdir") Then chkNoLogDir.Checked = True
+                        If commands(i).Contains("xerrorlba") Then chkXErrorLba.Checked = True
+                    Case "S"
+                        If commands(i).Contains("on") Then
+                            chkEnableAutosave.Enabled = True
+                            chkEnableAutosave.Checked = True
+                        Else
+                            chkEnableAutosave.Enabled = True
+                            chkEnableAutosave.Checked = False
+                        End If
+                End Select
+            End If
+        Next
+
+        isloading_devsettings = False
+    End Sub
+End Class
